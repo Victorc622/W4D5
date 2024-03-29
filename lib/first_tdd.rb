5e9a8406c34f6e9795a6c188952c26ef7a504a4f
Edison Li <47466335+edison4354@users.noreply.github.com>
2024-03-29 12:57:39 -0700

    Add rspec for methods my_transpose, stock_picker, and tower game

--- lib/first_tdd.rb
+++ lib/first_tdd.rb
@@ -8,9 +8,40 @@ class Array
     end
 
     def two_sum
-        i = self[0]
-        j = self[-1]
+        pairs = []
+        res = {}
 
-        
+        self.each_with_index do |ele, idx|
+            pairs << [res[0 - ele], idx] if res.include?(0 - ele)
+            res[ele] = idx
+        end
+        pairs.sort
     end
+end
+
+def my_transpose(matrix)
+    size = matrix.length
+    res = Array.new(size) { Array.new() }
+
+    (0...size).each do |i|
+        matrix.each do |sub_arr|
+            res[i] << sub_arr[i]
+        end
+    end
+    res
+end
+
+
+# [ 4, 5, 6, 3, 7, 2 ] -> [3, 4]
+
+def stock_picker(arr)
+    highest_pair = [0, 0]
+
+    (0...arr.length).each do |i|
+        (i+1...arr.length).each do |j|
+            highest_pair = [arr[i], arr[j]] if arr[j] - arr[i] > highest_pair[1] - highest_pair[0]
+        end
+    end
+
+    return [arr.find_index(highest_pair[0]), arr.find_index(highest_pair[1])]
 end
--- spec/first_tdd_spec.rb
+++ spec/first_tdd_spec.rb
@@ -1,19 +1,67 @@
 require 'first_tdd'
 
 describe Array do
-    describe "#my_uniq" do 
-        context 'removes duplicates from an array' do 
+    describe "#my_uniq" do
+        context 'removes duplicates from an array' do
             it 'should return a unique array' do
                 expect([1, 2, 1, 3, 3].my_uniq).to eq([1, 2, 3])
             end
         end
     end
-end
 
     describe "#two_sum" do
         context 'should return all pairs of positions where the elements at those positions sum to zero' do
             it 'should return 2d array of pairs' do
                 expect([-1, 0, 2, -2, 1].two_sum).to eq([[0, 4], [2, 3]])
+            end
+        end
+    end
+end
+
+describe "#my_transpose" do
+    it 'should accept a matrix as args' do
+        expect {my_transpose([[0, 1, 2], [3, 4, 5], [6, 7, 8]])}.to_not raise_error
+    end
+    context 'convert between row-oriented and column-oriented representations' do
+        it 'should return a converted matrix' do
+            expect(my_transpose([[0, 1, 2], [3, 4, 5], [6, 7, 8]])).to eq([[0, 3, 6], [1, 4, 7], [2, 5, 8]])
+        end
+    end
+end
+
+# [ 4, 5, 6, 3, 7, 2 ] -> [3, 4]
+describe "#stock_picker" do
+    it 'should accept an array as args' do
+        expect {stock_picker([4, 5, 6, 3, 7, 2])}.to_not raise_error
+    end
+    context 'outputs the most profitable pair of days on which to first buy the stock and then sell the stock' do
+        it 'should return a array of the pair of indices' do
+            expect(stock_picker([4, 5, 6, 3, 7, 2])).to eq([3, 4])
+        end
+    end
+end
+
+# [[1, 2, 3, 4], [], []] -> [[], [], [1, 2, 3, 4]]
+describe TowerGame do
+    subject(:game) { TowerGame.new }
+    describe "#initialize" do
+        context "create a new matrix grid" do
+            it "initializes a grid correctly" do
+                expect(game.grid).to eq([[1, 2, 3, 4], [], []])
+            end
+        end
+    end
+
+    describe '#move' do
+        it 'should take in a start and end arg' do
+            expect {game.move(0, 2)}.to_not raise_error
+        end
+
+        context 'should mutate the grid when you use #move' do
+            it 'should mutate and return new grid' do
+                game_2 = TowerGame.new
+                game_2.move(0, 2)
+                expect(game_2.grid).to eq([[2, 3, 4], [], [1]])
             end
         end
     end
